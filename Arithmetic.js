bc1f2b7a77d858883fa9dd98baacc7d9172d5521
ramyam75 <ramyarajeshwari2008@gmail.com>
2026-06-12 11:02:45 +0530

    Added Operators

---
+++ Arithmetic.js
@@ -0,0 +1,14 @@
+const a =10
+const b=20
+//Addition
+console.log (a+b);
+//subtraction
+console.log (a-b);
+//multiplication
+console.log (a*b);
+//division
+console.log (a/b);
+//modulus
+console.log (a%b);
+//exponentiation
+console.log (a**b);
