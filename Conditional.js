7ed402b594aa1f0637ddfb458dbf05fc59a20a10
ramyam75 <ramyarajeshwari2008@gmail.com>
2026-06-13 14:01:40 +0530

    Added Conditional.js

---
+++ Conditional.js
@@ -0,0 +1,14 @@
+var light = "Green";
+
+if (light == "Green") {
+    console.log("Go!");
+} 
+else if (light == "Yellow") {
+    console.log("Slow down!");
+} 
+else if (light == "Red") {
+    console.log("Stop!");
+} 
+else {
+    console.log("Invalid traffic light");
+}
